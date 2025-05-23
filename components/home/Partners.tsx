59bebaa798db2c5a556f5e30e55775ceeb29d1a0
papayaw007 <papa.adu-asare@ashesi.edu.gh>
2025-05-23 15:44:05 +0000

    updates on partners and board components

--- components/home/Partners.tsx
+++ components/home/Partners.tsx
@@ -8,11 +8,11 @@ function Partners() {
   return (
     <section className="mt-20 ">
       <Heading title="Partners" />
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 space-x-8 lg:space-x-2">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 space-x-8 lg:space-x-1">
         {partnersData.map((data) => (
           <div
             key={data.id}
-            className="shrink-0 px-4 md:px-6 lg:px-4 flex justify-center items-center space-x-0 space-y-0">
+            className="shrink-0 px-4 md:px-6 lg:px-1 flex justify-center items-center space-x-0 space-y-0">
             <Image
               src={data.image}
               alt={data.alt}
